1afcf5f811290061da975912e72e3a192a9eb1d0
Jigar <shahjigar185@gmail.com>
2021-07-03 18:56:01 +0530

    Implemeted grid view

---
+++ lib/screens/calci.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class Calculator extends StatefulWidget {
+  const Calculator({Key key}) : super(key: key);
+
+  @override
+  _CalculatorState createState() => _CalculatorState();
+}
+
+class _CalculatorState extends State<Calculator> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:SafeArea(
+        child: Column(
+          children: [
+            Container(
+              width:double.infinity,
+              constraints: BoxConstraints(
+                minHeight: 200.0,
+                maxHeight: 600.0
+              ),
+
+              child:Text('Result Container'),
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.blueAccent)
+              ),
+            ),
+            Expanded(
+              child: GridView.count(
+                primary: false,
+                padding: const EdgeInsets.all(20),
+                physics: new NeverScrollableScrollPhysics(),
+                crossAxisSpacing: 10,
+                mainAxisSpacing: 10,
+                crossAxisCount: 2,
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    child: const Text("He'd have you all unravel at the"),
+                    color: Colors.teal[100],
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    child: const Text('Heed not the rabble'),
+                    color: Colors.teal[200],
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    child: const Text('Sound of screams but the'),
+                    color: Colors.teal[300],
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    child: const Text('Who scream'),
+                    color: Colors.teal[400],
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    child: const Text('Revolution is coming...'),
+                    color: Colors.teal[500],
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    child: const Text('Revolution, they...'),
+                    color: Colors.teal[600],
+                  ),
+                ],
+              ),
+            )
+
+          ],
+
+        ),
+      )
+    );
+  }
+}
